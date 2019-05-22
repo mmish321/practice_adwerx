697e8c86a4dc339887175f1b75295eedacdb3096
Michelle Mao <michellemao@Michelles-MacBook-Pro.local>
2019-05-22 12:33:26 -0400

    Finished comment editing capabilities

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,34 @@
+class CommentsController < ApplicationController
+	def create
+		@article = Article.find(params[:article_id])
+		@comment = @article.comments.create(comment_params)
+		if @comment.save
+			redirect_to article_path(@article)
+		end
+	end
+	def destroy
+		@article = Article.find(params[:article_id])
+		@comment = @article.comments.find(params[:id])
+		@comment.destroy
+		redirect_to article_path(@article)
+	end
+	def edit
+		puts params
+		@article = Article.find(params[:article_id])
+		@comment = @article.comments.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:article_id])
+		@comment = @article.comments.find(params[:id])
+		if @comment.update(comment_params)
+			redirect_to @article
+		else
+			render "edit"
+		end
+	end
+	private
+		def comment_params
+			params.require(:comment).permit(:commenter, :body)
+		end
+end
